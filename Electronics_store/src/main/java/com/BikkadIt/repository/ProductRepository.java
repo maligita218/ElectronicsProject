5ca39f2b38cccaa31c79c6a909f717496e2e36b5
maligita218 <maligita218@gmail.com>
2023-04-08 18:57:16 +0530

    Changes Done with ProductRepository

--- Electronics_store/src/main/java/com/BikkadIt/repository/ProductRepository.java
+++ Electronics_store/src/main/java/com/BikkadIt/repository/ProductRepository.java
@@ -1,7 +1,17 @@
 package com.BikkadIt.repository;
 
+import com.BikkadIt.dto.ProductDto;
 import com.BikkadIt.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    Page<Product> findByTitleContaining(String subTitle,Pageable pageable);
+
+    Page<Product> findByLiveTrue(Pageable pageable);
+
 }
